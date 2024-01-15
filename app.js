b4861120ecc565d13f25579c4c68148d897e67a4
pak <paksuetshih@gmail.com>
2024-01-15 22:37:23 +1100

    added grey out overlay and loading spinner
    use var instead of let for browser compatibility
    
    synced vsl3 html

--- app.js
+++ app.js
@@ -2,26 +2,23 @@
 todo URL,URLSearchParams, not supported by older browser. find replacement lib?
  */
 document.addEventListener('DOMContentLoaded', function() {
-    // Select all anchor elements with the attribute 'merge-url-params'
-    // import(window.homePath + '/config.js')
-    //     .then(configModule => {
-    // Now you can use properties from the imported module
-    let hopLink = window.vendorHopLink;
+    var hopLink = window.vendorHopLink;
     console.log('vendorHopLink', hopLink);
     if (!hopLink) {
         console.error('cannot continue as no vendorHopLink specified in config.js')
         return;
     }
-    let linksWithMergeParams = document.querySelectorAll('a.cta');
+    var linksWithMergeParams = document.querySelectorAll('a.cta');
     // Loop over each selected element and update the href attribute
-    linksWithMergeParams.forEach(function(link) {
-        console.log('link.href', link.href);
-        let allParams = new URLSearchParams();
+    linksWithMergeParams.forEach(function(linkElement) {
+        console.log('link.href', linkElement.href);
+        // merge hoplink params
+        var allParams = new URLSearchParams();
         new URL(hopLink).searchParams.forEach(function (value, key) {
             // cmc_vid from vendor hoplink will be replaced later
             allParams.set(key, value);
         })
-        new URL(link.href).searchParams.forEach(function(value, key) {
+        new URL(linkElement.href).searchParams.forEach(function(value, key) {
             //this contains cmc replaced cmc_vid
             allParams.set(key, value);
         });
@@ -29,43 +26,24 @@ document.addEventListener('DOMContentLoaded', function() {
             // params from current URL
             allParams.set(key, value);
         })
-        let finalUrl = new URL(hopLink);
+        var finalUrl = new URL(hopLink);
         finalUrl.search = allParams.toString();
-        link.href = finalUrl.toString();
-        console.log('final URL', finalUrl.toString())
-    });
-    // })
-    // .catch(error => {
-    //     console.error('Error loading config:', error);
-    // });
-}, false);
+        linkElement.href = finalUrl.toString();
+        console.log('final URL', finalUrl.toString());
 
-/*
-//todo flickering when this runs
-function includeHTML() {
-    var z, i, elmnt, file, xhttp;
-    /!* Loop through a collection of all HTML elements: *!/
-    z = document.getElementsByTagName("*");
-    for (i = 0; i < z.length; i++) {
-        elmnt = z[i];
-        /!*search for elements with a certain atrribute:*!/
-        file = elmnt.getAttribute("include-html-file");
-        if (file) {
-            /!* Make an HTTP request using the attribute value as the file name: *!/
-            xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() {
-                if (this.readyState == 4) {
-                    if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-                    if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-                    /!* Remove the attribute, and call this function once more: *!/
-                    elmnt.removeAttribute("include-html-file");
-                    includeHTML();
-                }
-            }
-            xhttp.open("GET", file, true);
-            xhttp.send();
-            /!* Exit the function: *!/
-            return;
-        }
-    }
-}*/
+        // show spinner
+        linkElement.addEventListener('click', function() {
+            var spinner = document.getElementById('loadingSpinner');
+            var overlay = document.getElementById('overlay');
+            spinner.classList.remove('hidden');
+            overlay.style.display = 'block';
+
+            // Simulate some asynchronous task (e.g., fetching data)
+            setTimeout(function() {
+                // Hide the loading spinner after the task is complete (simulate loading completion)
+                spinner.classList.add('hidden');
+                overlay.style.display = 'none';
+            }, 2500); // Adjust the time based on your actual asynchronous task duration
+        });
+    });
+}, false);
