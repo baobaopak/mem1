cdf4379c8380dbeb17192c372ce319391fd1e490
pak <paksuetshih@gmail.com>
2024-01-15 20:18:20 +1100

    simplified config.js

--- app.js
+++ app.js
@@ -1,82 +1,43 @@
-var UrlUtils = UrlUtils || {};
-UrlUtils.getURLParameters = function (url) {
-    var paramsStr = url.split('?', 2);
-    if (paramsStr.length < 2) return [];
-    var params = [], search = /([^&=]+)=?([^&]*)/g, query = paramsStr[1];
-    var match = ""
-    while (match = search.exec(query)) {
-        if (match.length >= 1) {
-            params.push({key: match[1], value: match[2],})
-        }
-    }
-    return params
-};
-UrlUtils.generateUrlWithParams = function (url, data) {
-    if (!url) return '';
-    var params = [], urlObj = url.split('?', 1)[0];
-    data.forEach(function (dataItem) {
-        params.push(dataItem.key + '=' + dataItem.value)
-    });
-    return params.length ? urlObj + '?' + params.join('&') : url
-};
-UrlUtils.getUrlWithoutAnchors = function (url) {
-    var urlAndAnchor = url.split('#', 1);
-    return urlAndAnchor[0]
-};
-UrlUtils.getAnchorName = function (url) {
-    var urlAndAnchor = url.split('#', 2);
-    return urlAndAnchor.length > 1 ? '#' + urlAndAnchor[1] : ''
-};
-//todo simplify
-UrlUtils.mergeUrlWithRequestParams = function (url) {
-    var urlWithoutAnchors = UrlUtils.getUrlWithoutAnchors(url);
-    var elementAnchor = UrlUtils.getAnchorName(url);
-    var urlParams = UrlUtils.getURLParameters(location.search);
-    var urlAnchor = location.hash;
-    var localParameters = UrlUtils.getURLParameters(urlWithoutAnchors);
-    localParameters = localParameters.concat(urlParams);
-    var resultUrl = UrlUtils.generateUrlWithParams(urlWithoutAnchors, localParameters);
-    var anchor = elementAnchor ? elementAnchor : urlAnchor;
-    return resultUrl + anchor
-};
-
+/*
+todo URL,URLSearchParams, not supported by older browser. find replacement lib?
+ */
 document.addEventListener('DOMContentLoaded', function() {
     // Select all anchor elements with the attribute 'merge-url-params'
-    console.log("window.homePath", window.homePath)
-    import(window.homePath + '/config.js')
-        .then(configModule => {
-            // Now you can use properties from the imported module
-            console.log('vendorHopLink', configModule.vendorHopLink);
-            if (!configModule.vendorHopLink) {
-                console.error('cannot continue as no vendorHopLink specified in ', window.homePath + '/config.js')
-                return;
-            }
-            let linksWithMergeParams = document.querySelectorAll('a.cta');
-            // Loop over each selected element and update the href attribute
-            linksWithMergeParams.forEach(function(link) {
-                console.log('link.href', link.href);
-                let allParams = new URLSearchParams();
-                new URL(configModule.vendorHopLink).searchParams.forEach(function (value, key) {
-                    // cmc_vid from vendor hoplink will be replaced later
-                    allParams.set(key, value);
-                })
-                new URL(link.href).searchParams.forEach(function(value, key) {
-                    //this contains cmc replaced cmc_vid
-                    allParams.set(key, value);
-                });
-                new URLSearchParams(location.search).forEach(function (value, key) {
-                    // params from current URL
-                    allParams.set(key, value);
-                })
-                let finalUrl = new URL(configModule.vendorHopLink);
-                finalUrl.search = allParams.toString();
-                link.href = finalUrl.toString();
-                console.log('final URL', finalUrl.toString())
-            });
+    // import(window.homePath + '/config.js')
+    //     .then(configModule => {
+    // Now you can use properties from the imported module
+    let hopLink = window.vendorHopLink;
+    console.log('vendorHopLink', hopLink);
+    if (!hopLink) {
+        console.error('cannot continue as no vendorHopLink specified in config.js')
+        return;
+    }
+    let linksWithMergeParams = document.querySelectorAll('a.cta');
+    // Loop over each selected element and update the href attribute
+    linksWithMergeParams.forEach(function(link) {
+        console.log('link.href', link.href);
+        let allParams = new URLSearchParams();
+        new URL(hopLink).searchParams.forEach(function (value, key) {
+            // cmc_vid from vendor hoplink will be replaced later
+            allParams.set(key, value);
         })
-        .catch(error => {
-            console.error('Error loading config:', error);
+        new URL(link.href).searchParams.forEach(function(value, key) {
+            //this contains cmc replaced cmc_vid
+            allParams.set(key, value);
         });
+        new URLSearchParams(location.search).forEach(function (value, key) {
+            // params from current URL
+            allParams.set(key, value);
+        })
+        let finalUrl = new URL(hopLink);
+        finalUrl.search = allParams.toString();
+        link.href = finalUrl.toString();
+        console.log('final URL', finalUrl.toString())
+    });
+    // })
+    // .catch(error => {
+    //     console.error('Error loading config:', error);
+    // });
 }, false);
 
 /*
--- lp1vsl1/config.js
+++ lp1vsl1/config.js
@@ -1,1 +1,1 @@
-export const vendorHopLink = 'https://hop.clickbank.net/?affiliate=pakpak&vendor=neurozoom&pid=quiz&tid=[cmc_vid]'
+window.vendorHopLink = 'https://hop.clickbank.net/?affiliate=pakpak&vendor=neurozoom&pid=quiz&tid=[cmc_vid]'
