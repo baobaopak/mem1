2dd93ea0e34aa05751bcbf76552ca326b39dfb83
pak <paksuetshih@gmail.com>
2024-01-15 22:45:56 +1100

    fixed config for vsl3

--- lp1vsl3/config.js
+++ lp1vsl3/config.js
@@ -1,1 +1,1 @@
-export const vendorHopLink = 'https://hop.clickbank.net/?affiliate=pakpak&vendor=neurozoom&pid=paradoxical&tid=[cmc_vid]'
+window.vendorHopLink = 'https://hop.clickbank.net/?affiliate=pakpak&vendor=neurozoom&pid=paradoxical&tid=[cmc_vid]'
