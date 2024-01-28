c5aa67bd07f867e56f3f5c7f6b629d26f0950ea7
pak <paksuetshih@gmail.com>
2024-01-28 14:56:11 +1100

    removed unused files

--- clickmagic_cmc_mem1.js
+++
@@ -1,8 +0,0 @@
-//todo is uid, hid always the same for my account?
-window.clickmagick_cmc = {
-    uid: '190723',
-    hid: '928018351',
-    cmc_project: 'Memory1',
-    log_action: '.cta',
-    vid_info: 'on',
-}
